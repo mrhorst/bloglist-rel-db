0761957f953b8a0207a599a005d0e1425065b5a8
mrhorst <mhorst31@gmail.com>
2025-11-03 23:48:05 -0500

    fix `docker-compose.dev.yml` and add dialect to sequelize

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 require('dotenv').config()
 const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize(process.env.DATABASE_URL)
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+})
 
 const main = async () => {
   try {
